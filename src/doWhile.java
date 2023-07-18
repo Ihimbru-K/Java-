b4dc6332b23f65a4ec050b95c54bef90a2653f97
Ihimbru-k <kanihims25@gmail.com>
2023-07-18 17:02:32 +0100

    do-while loop

---
+++ src/doWhile.java
@@ -0,0 +1,16 @@
+public class doWhile {
+    public static void main(String[] args){
+
+        // Initialize a variable i to 1
+        int i = 1;
+
+        // Start a do-while loop that will execute at least once
+        do{
+            // Print the value of i
+            System.out.println(i);
+            // Increment the value of i by 1
+            i++;
+        }while(i <= 5); // Check if i is less than or equal to 5, and continue looping if true
+
+    }
+}
