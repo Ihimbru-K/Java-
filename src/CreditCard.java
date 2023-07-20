891c94a5967e8c7ca97ac034b87783a92f86f4dd
Ihimbru-k <kanihims25@gmail.com>
2023-07-20 07:49:18 +0100

    credit card example

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -51,6 +51,23 @@ public class CreditCard {
 
         CreditCard[] wallet = new CreditCard[3];
         wallet[0] = new CreditCard("Matt","Bamcul", "67745 1140", 100000, 4000);
+        wallet[1] = new CreditCard("Kboy","Camcul","68224",120000, 45000);
+        wallet[2] = new CreditCard("Kb", "Ntaccul", "693622", 190000, 45000);
+
+
+        for(int val = 0; val <= 16; val++){
+            wallet[0].charge(3*val);
+            wallet[1].charge(2*val);
+            wallet[0].charge(val);
+        }
+
+        for(CreditCard myCard : wallet){
+            CreditCard.printInfo(myCard);
+            while (myCard.getBalance() >3000){
+                myCard.makePayment(2900);
+                System.out.println("Your account balance is: " +myCard.getBalance());
+            }
+        }
 
 
     }
--- src/ReadingInputOutput.java
+++ src/ReadingInputOutput.java
@@ -10,7 +10,11 @@ public class ReadingInputOutput {
         System.out.println("Enter your age");
         int myAge = age.nextInt();
 
-        System.out.println(myName+" you are "+myAge+" years old");
+        Scanner school = new Scanner(System.in);
+        System.out.println("Enter your school");
+        String mySchool = school.next();
+
+        System.out.println(myName+" you are "+myAge+" years old" + "and you school in"+mySchool);
     }
 
 }
