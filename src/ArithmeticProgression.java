cb6f237e5f5730c9f00f822aa18e906ec79f709d
Ihimbru-k <kanihims25@gmail.com>
2023-08-13 18:35:20 +0100

    more on inheritance

---
+++ src/ArithmeticProgression.java
@@ -0,0 +1,21 @@
+public class ArithmeticProgression extends Progression{
+    protected long increment;
+    public ArithmeticProgression(){
+        this(1,0);
+    }
+
+    public ArithmeticProgression(long step){
+        this(step, 0);
+    }
+
+
+    public ArithmeticProgression(long step, long start) {
+        super(start);
+        increment = step;
+    }
+
+    protected void Advance(){
+        current += increment;
+    }
+    
+}
