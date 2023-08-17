301e82136803dc308910f617482d93caca2983db
Ihimbru-k <kanihims25@gmail.com>
2023-08-17 15:47:55 +0100

    more exercises

--- src/PrintMultiplicationTableFrom1to10.java
+++ src/PrintMultiplicationTableFrom1to10.java
@@ -1,2 +1,12 @@
 public class PrintMultiplicationTableFrom1to10 {
+
+    public static void main(String[] args){
+        int i, j =0;
+        for(i = 1; i <= 10; i++){
+            for( j = 1; j<=10; j++){
+                System.out.println(i+"*"+j+"="+(i*j));
+            }
+            System.out.println("   ");
+        }
+    }
 }
