7dd80fae9689a7be5e9849e305ec290f54f72865
Ihimbru-k <kanihims25@gmail.com>
2023-08-17 15:54:29 +0100

    ex5

---
+++ src/SumOfNumbersFrom1to10.java
@@ -0,0 +1,13 @@
+public class SumOfNumbersFrom1to10 {
+
+    public static void main(String[] args){
+        int sum = 0, i = 0;
+        while(i< 10){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+
+
+}
