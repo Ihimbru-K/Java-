52cf0d2fff585c4f3f828d4a33ee3e17ae4155fd
Ihimbru-k <kanihims25@gmail.com>
2023-08-17 15:36:21 +0100

    3 more exercises

---
+++ src/MultiplicationTableWith7.java
@@ -0,0 +1,8 @@
+public class MultiplicationTableWith7 {
+    public static void main(String[ ] args){
+        int num = 7;
+        for(int i = 1; i<=10; i++){
+            System.out.println("7 *"+i+" = "+ (num * i));
+        }
+    }
+}
