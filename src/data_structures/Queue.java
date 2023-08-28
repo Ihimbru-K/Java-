361a172b6f928771a6916af0f9278f66e5b6e1f8
Ihimbru-k <kanihims25@gmail.com>
2023-08-28 01:45:36 +0100

    implemented queue data structure

--- src/data_structures/Queue.java
+++
@@ -1,5 +0,0 @@
-package data_structures;
-
-public class Queue {
-    
-}
---
+++ src/data_structures/Queues.java
@@ -0,0 +1,22 @@
+package data_structures;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Queues {
+
+    public static void main(String[] args){
+        Queue<People> people = new LinkedList<>();
+        people.add(new People("Ihims",22));
+        people.add(new People("Kanyimi",21));
+        people.add(new People("Paul",20));
+
+        System.out.println(people.peek());
+        people.peek();
+        System.out.println(people.poll()); //remove first element which is
+        System.out.println(people.size());
+
+    }
+    static record People(String name, int age){};
+}
