af62d0e1b543cfbf129bf3ffaebcda2768f1ea80
Ihimbru-k <kanihims25@gmail.com>
2023-08-31 16:46:56 +0100

    iterating through set data structure

--- src/data_structures/TheSet.java
+++ src/data_structures/TheSet.java
@@ -1,11 +1,10 @@
 package data_structures;
 
 import java.util.HashSet;
-import java.util.ListIterator;
 import java.util.Set;
 
 public class TheSet {
-    public static void main(){
+    public static void main(String[] args){
 
         Set<People> peopleSet = new HashSet<>();
         peopleSet.add(new People("Mike"));
@@ -14,7 +13,7 @@ public class TheSet {
         peopleSet.add(new People("Jason")); // set data structure doesn't recognize duplicates
         // prints elements of set in any order because element of a set aren't stored in any order
         peopleSet.forEach(System.out::println);
-        ListIterator<People> peopleListIterator = peopleSet.peopleListIterator();
+
     }
     record People(String names){}
 }
