aefb105c2cbc6a0d7ad0326176b7a6285817d3ac
Ihimbru-k <kanihims25@gmail.com>
2023-09-02 17:42:48 +0100

    second exercise on arrays

---
+++ src/AverageOfNumbersInArray.java
@@ -0,0 +1,22 @@
+public class AverageOfNumbersInArray {
+
+    public static double ArrayAvg(int[] arr) {
+        final int n = arr.length;
+        int sum = 0;
+        double avg = 0;
+        for (int i : arr
+        ) {
+            sum += i;
+            avg = (double) sum / n;
+        }
+        return avg;
+    }
+
+    public static void main(String[] args){
+        int[] testArr = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("average of elements of array is "+ ArrayAvg(testArr));
+
+    }
+
+
+}
