3547c0f091d05fad59d1043ffe393e6e09e664b3
Ihimbru-k <kanihims25@gmail.com>
2023-09-02 17:53:44 +0100

    exercise 12 of coding exercises

---
+++ src/ArrayOfPositiveNumOnly.java
@@ -0,0 +1,22 @@
+public class ArrayOfPositiveNumOnly {
+
+
+    public static int[] ArrayOfPositive(int[] Array){
+
+        for (int i:Array
+             ) {
+            if(
+                    i%2 !=0
+            ){
+
+            }
+        }
+        return Array;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
--- src/AverageOfNumbersInArray.java
+++ src/AverageOfNumbersInArray.java
@@ -1,6 +1,8 @@
+import org.jetbrains.annotations.NotNull;
+
 public class AverageOfNumbersInArray {
 
-    public static double ArrayAvg(int[] arr) {
+    public static double ArrayAvg(int @NotNull [] arr) {
         final int n = arr.length;
         int sum = 0;
         double avg = 0;
@@ -15,8 +17,9 @@ public class AverageOfNumbersInArray {
     public static void main(String[] args){
         int[] testArr = {1,2,3,4,5,6,7,8,9,10};
         System.out.println("average of elements of array is "+ ArrayAvg(testArr));
-
     }
+
+
 
 
 }
