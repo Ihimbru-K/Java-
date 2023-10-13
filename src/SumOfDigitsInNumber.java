4f382e3d80be51787b07de084191c674a91fca78
Ihimbru-k <kanihims25@gmail.com>
2023-10-13 16:35:08 +0100

    more exercises

--- src/SumOfDigitsInNumber.java
+++ src/SumOfDigitsInNumber.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SumOfDigitsInNumber {
+public class SumOfDigitsInNumber {
+
+    public static int sumOfDigitsInNum(int num) {
+        int storeBrokenDigitSum = 0;
+        while (num > 0) {
+
+            storeBrokenDigitSum += num % 10;
+            num /= 10;
+        }
+        return storeBrokenDigitSum;
+    }
+
+    public static void main(String[] Args){
+        int num = 4444;
+        System.out.println("Sum of integers in 40 is "+sumOfDigitsInNum(num));
+
+    }
 }
