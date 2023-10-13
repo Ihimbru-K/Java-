d2391de55ed4348256bacc79ef3367ea27ebd960
Ihimbru-k <kanihims25@gmail.com>
2023-10-13 16:18:21 +0100

    completed exercise 16

--- src/PrimeNumTest.java
+++ src/PrimeNumTest.java
@@ -1,4 +1,24 @@
 public class PrimeNumTest {
+// ex 16
+    public static boolean PrimeTest(int num){
+        if(num <= 1)
+            return false;
 
-    
+        for (int i =2; i < Math.sqrt(num) ; i++) {
+            if(num %i == 0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+
+        System.out.println(PrimeTest(13));
+    }
+
+
+
+
 }
