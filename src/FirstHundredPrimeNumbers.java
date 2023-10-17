b8ca4bc6b41ce83706f58a3dc21ec5d07684c751
Ihimbru-k <kanihims25@gmail.com>
2023-10-17 11:50:42 +0100

    .

--- src/FirstHundredPrimeNumbers.java
+++ src/FirstHundredPrimeNumbers.java
@@ -1,15 +1,31 @@
+
 public class FirstHundredPrimeNumbers {
-
     public static void FirstHundredPrime() {
-        
+        int count = 0; // Variable to keep track of the number of prime numbers found
+        int number = 2; // Starting number to check for primality
 
+        while (count < 100) {
+            boolean isPrime = true;
 
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(number);
+                count++;
+            }
+
+            number++;
+        }
+    }
+    public static void main(String[]args){
+
+        FirstHundredPrime();
 
     }
-
-    public static void main(String[] args){
-
-    }
-
 
 }
